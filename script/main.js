ee7bea9ddf7923ade9e575b0fbd3df126bfd3d3f
Artem <OstapT5@gmail.com>
2023-10-20 11:13:04 +0200

    info modal

--- script/main.js
+++ script/main.js
@@ -13,3 +13,16 @@ dropdownsArr.forEach(el => {
     
 })
 
+document.querySelector('#open-info-modal').addEventListener('click', () => {
+    document.querySelector('#modal-info').classList.add('show');
+})
+
+//close modal
+document.querySelectorAll('.modal').forEach(el => {
+    el.addEventListener('click', () => {
+        el.classList.remove('show');
+    })
+})
+document.querySelectorAll('.modal__container').forEach(el => {
+    el.addEventListener('click', (e) => e.stopPropagation());
+})
